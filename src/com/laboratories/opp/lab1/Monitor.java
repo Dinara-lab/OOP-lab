575b28cce890d5c2f5a54acdf2038014793f9395
Dinara-lab <69628270+Dinara-lab@users.noreply.github.com>
2020-09-19 21:40:58 +0300

    Update Monitor.java

--- src/com/laboratories/opp/lab1/Monitor.java
+++ src/com/laboratories/opp/lab1/Monitor.java
@@ -4,8 +4,8 @@ public class Monitor {
     public static void main (String [] args ){
         MonitorProperties m1 = new MonitorProperties("red", 59, 1080);
         MonitorProperties m2 = new MonitorProperties("blue",58,2160);
-        System.out.println("Monitor 1:"+ m1.showInfo());
-        System.out.println("Monitor 2:" +  m2.showInfo());
+        System.out.println("Monitor 1:" + m1.showInfo());
+        System.out.println("Monitor 2:" + m2.showInfo());
         if (m1.showInfo().equals(m2.showInfo())){
             System.out.println("The properties are equal");
         }else {
